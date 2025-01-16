3b5ec19eb728a8c2c2c86b09649df7b3dadeb7f4
AmithBV0606 <amithrao0606@gmail.com>
2025-01-16 07:59:52 +0530

    Hero section completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-5xl font-bold underline font-circular-web text-violet-300">Hello world!</h1>
+    <div className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+
+      <section className="z-0 min-h-screen bg-blue-500"/>
     </div>
   );
 };
